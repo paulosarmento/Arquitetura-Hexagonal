ec720331131ee94c5aa4444b492872c988c8e7dd
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-09 07:55:44 -0300

    Implementando CLI com Cobra

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	_ "github.com/mattn/go-sqlite3"
-	db2 "github.com/paulosarmento/Arquitetura-Hexagonal/adapters/db"
-	"github.com/paulosarmento/Arquitetura-Hexagonal/application"
-)
+import "github.com/paulosarmento/Arquitetura-Hexagonal/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemplo", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
