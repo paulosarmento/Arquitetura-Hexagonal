6d3180ede6fd0d17246738ee36501665c0684d01
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-09 08:53:04 -0300

    Iniciando implementaçao dos handlers

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/paulosarmento/Arquitetura-Hexagonal/application"
+)
+
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+	r.Handle("/product/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+}
+
+func getProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Context-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+	})
+}
